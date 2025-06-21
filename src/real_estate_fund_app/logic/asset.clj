3cb0118cf371dd63f3ed65fb06895a4b67baf408
Brenno Martins <brennomartins@hotmail.com>
2025-06-20 23:39:59 -0300

    refactoring update values from recommendation

--- src/real_estate_fund_app/logic/asset.clj
+++ src/real_estate_fund_app/logic/asset.clj
@@ -27,11 +27,6 @@
   [percent-recommendation percent_current]
   ( - percent-recommendation percent_current))
 
-;(defn return-percent-current
-;  [value sum-value-asset]
-;  (* 100 (.divide value sum-value-asset RoundingMode/HALF_UP))
-;  )
-;
 (defn return-percent-current
   [value sum-value-asset]
   (-> (bigdec value)
@@ -43,14 +38,10 @@
   (let [total-all-asset-avg (* perc-diff-recommendation sum-value-avg-asset)]
     (/ total-all-asset-avg average-price)))
 
-
-; TODO para o calculo do sum esta considerando so os que tao na base de dados e nao o novo q ta vindoo
 (s/defn return-calculated-values :- model.asset/asset-schema
   "Calculate the values for the asset based on the quotation and average price."
   [quotation :- s/Num
    body :- model.asset/asset-schema
-   sum-value-asset :- s/Num
-   sum-value-avg-asset :- s/Num
    ]
   (let [average-price (:value-average-price-asset body)
         quantity (:quantity-asset body)
@@ -58,22 +49,16 @@
         value (return-value-asset quantity quotation)
         value-total-avg (return-value-total-avg-asset quantity average-price)
         profit (return-profit-asset value value-total-avg)
-        quantity-fix 0
-        perc-diff-recommendation 0
-        percent_current (return-percent-current value sum-value-asset)
-        ;perc-diff-recommendation (return-perc-diff-recommendation (:percent-recommendation body) percent_current)
-        ;quantity-fix (return-quantity-fix perc-diff-recommendation sum-value-avg-asset average-price)
         ]
     (-> body
         (assoc :quotation-asset quotation)
         (assoc :index-asset index)
         (assoc :value-total-average-price-asset value-total-avg)
         (assoc :profit-asset profit)
-        (assoc :percent-current percent_current)
-        (assoc :percent-difference-recommendation perc-diff-recommendation)
-        (assoc :quantity-fix quantity-fix)
+        (assoc :percent-current 0)
+        (assoc :percent-difference-recommendation 0)
+        (assoc :quantity-fix 0)
         (assoc :value-asset value))))
-
 
 (defn return-sum-value-asset
   [assets]
