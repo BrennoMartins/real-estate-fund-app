0290ed3906d1f145e2fd541fe264eec4b6a623aa
Brenno Martins <brennomartins@hotmail.com>
2025-07-21 18:27:33 -0300

    remove TODO comments and improve asset query function documentation

--- src/real_estate_fund_app/diplomatic/db/asset.clj
+++ src/real_estate_fund_app/diplomatic/db/asset.clj
@@ -1,12 +1,10 @@
 (ns real-estate-fund-app.diplomatic.db.asset
   (:require [clojure.java.jdbc :as jdbc]
             [real-estate-fund-app.model.asset :as model.asset]
-            [schema.core :as s]
-            [real-estate-fund-app.diplomatic.db.financialdb :as diplomatic.db.financialdb]))
+            [schema.core :as s]))
 
-; TODO colocar a saida do metodo no formato do schema
 (s/defn return-all-assets-db :- model.asset/asset-schema
-  "Return all asset."
+  "Return all assets."
   [db table]
   (jdbc/query db
               [(str "SELECT * FROM " table " ORDER BY id_asset DESC")]))
@@ -17,7 +15,3 @@
   (println id)
   (jdbc/query db
               [(str "SELECT * FROM " table " WHERE id_asset = ?") id]))
-
-
-; TODO Lembrar que tenho que converter o retorno para o formato do schema -
-(return-all-assets-db diplomatic.db.financialdb/db "real_estate_fund")
